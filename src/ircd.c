13b300dd1622b32fa65a229ac6f273c45cff10c4
Nate Book <nmbook@gmail.com>
2014-04-12 19:01:30 -0400

    Gotta remember what the headers it needs are, GOT TO GO.

--- src/ircd.c
+++ src/ircd.c
@@ -4,9 +4,19 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <unistd.h>
 #include "ircd.h"
 
+#define IRCD_BACKLOG 10
+
 struct ircd {
+    // listener socket (TCP accept())
+    int listener;
+    //
 };
 
 ircd_t *ircd_new() {
@@ -25,6 +35,38 @@ ircd_free(ircd_t *ircd) {
 
 void
 ircd_start(ircd_t *ircd) {
+    struct addrinfo hints;
+    struct addrinfo *result;
+
+    memset(hints, 0, sizeof(hints));
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET6;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
+    // rest 0
+
+    int status = getaddrinfo(NULL, "ircd", &hints, &result);
+    if (!status) {
+        printf("getaddrinfo: ");
+        puts(gai_strerror(status));
+        return;
+    }
+
+    ircd->listener = socket(AF_INET6, SOCK_STREAM, 0);
+    if (ircd->listener < 0) {
+        perror("socket");
+        return;
+    }
+
+    if (bind(ircd->listener, result->ai_addr, result->ai_addrlen) < 0) {
+        perror("bind");
+        return;
+    }
+
+    if (listen(ircd->listener, IRCD_BACKLOG) > 0) {
+        perror("listen");
+        return;
+    }
 }
 
 void
