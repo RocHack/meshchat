2061d15c63d84d1bdc574706d13db2b5503f0a98
Nate Book <nmbook@gmail.com>
2014-04-13 00:04:37 -0400

    More structures

--- src/ircd.c
+++ src/ircd.c
@@ -11,11 +11,38 @@
 #include <unistd.h>
 #include <string.h>
 #include "ircd.h"
+#include "meshchat.h"
+
+struct irc_session {
+    // fd socket (TCP recv())
+    int fd;
+    // link
+    struct irc_session *next;
+};
+
+struct irc_user {
+    char name[MESHCHAT_NAME_LEN]; // 9
+    char username[MESHCHAT_FULLNAME_LEN]; // 32
+    char realname[MESHCHAT_FULLNAME_LEN]; // 32
+    char host[MESHCHAT_HOST_LEN]; // 63
+    struct irc_user *next;
+};
+
+struct irc_channel {
+    char name[MESHCHAT_CHANNEL_LEN]; // 50
+    char topic[MESHCHAT_MESSAGE_LEN]; // 512
+    struct irc_user *user_list;
+};
 
 struct ircd {
-    // listener socket (TCP accept())
-    int listener;
-    //
+    // fd socket (TCP accept())
+    int fd;
+    char nick[MESHCHAT_NAME_LEN]; // 9
+    char username[MESHCHAT_FULLNAME_LEN]; // 32
+    char realname[MESHCHAT_FULLNAME_LEN]; // 32
+    // connected sessions (LL)
+    struct irc_session *session_list;
+    struct irc_channel *channel_list;
 };
 
 ircd_t *ircd_new() {
@@ -24,6 +51,10 @@ ircd_t *ircd_new() {
 		perror("calloc");
 		return NULL;
 	}
+
+    ircd->fd = -1;
+    ircd->session_list = NULL;
+    ircd->channel_list = NULL;
 
 	return ircd;
 }
@@ -51,16 +82,20 @@ ircd_start(ircd_t *ircd) {
         return;
     }
 
-    ircd->listener = socket(AF_INET6, SOCK_STREAM, 0);
-    if (ircd->listener < 0) {
+    ircd->fd = socket(result->ai_family, result->ai_socktype, 0);
+    if (ircd->fd < 0) {
         perror("socket");
+        freeaddrinfo(result);
         return;
     }
 
     if (bind(ircd->listener, result->ai_addr, result->ai_addrlen) < 0) {
         perror("bind");
+        freeaddrinfo(result);
         return;
     }
+
+    freeaddrinfo(result);
 
     if (listen(ircd->listener, IRCD_BACKLOG) > 0) {
         perror("listen");
@@ -71,6 +106,7 @@ ircd_start(ircd_t *ircd) {
 void
 ircd_add_select_descriptors(ircd_t *mc, fd_set *in_set,
 		fd_set *out_set, int *maxfd) {
+    
 }
 
 void
