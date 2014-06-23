2dfdd490b730025981fe0404f75f7458fd30851c
Cy <whatever>
2014-06-22 22:27:02 -0700

    Adding main.mk (the new name for Makefile)
    It should actually compile now ._.

---
+++ main.mk
@@ -0,0 +1,33 @@
+BIN = meshchat
+SRC = $(wildcard src/*.c)
+SRC += $(wildcard deps/*/*.c)
+OBJ = $(SRC:.c=.o)
+CFLAGS += -Ideps -Wall -pedantic
+CFLAGS += -std=gnu99
+
+all: $(BIN)
+
+ifdef INTERNAL_LIBUV
+CFLAGS += -Ideps/libuv/include
+LDFLAGS += deps/libuv/.libs/libuv.a -lc -lpthread
+else
+LDFLAGS += -luv
+endif
+
+$(BIN):: $(OBJ)
+	${CC} -o $@ $^ ${LDFLAGS}
+
+.c.o:
+	${CC} -c ${CFLAGS} $< -o $@
+
+install: all
+	install -m 0755 ${BIN} ${DESTDIR}${BINDIR}
+
+uninstall:
+	rm -f ${DESTDIR}${BINDIR}/${BIN}
+
+clean:
+	rm -f $(BIN) $(OBJ)
+
+.PHONY: all install uninstall
+
